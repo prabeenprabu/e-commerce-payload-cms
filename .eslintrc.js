481e912d210693220668f01e237c8c21720814ed
prabeen <prabeenprabu@gmail.com>
2024-08-21 11:53:29 +0530

    basic css style implimented

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
+  endOfLine: 'crlf',
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    // 'prettier/prettier': ['error', { endofLine: 'auto' }],
+  },
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,15 @@ import { Header } from './_components/Header'
 import { Providers } from './_providers'
 import { InitTheme } from './_providers/Theme/InitTheme'
 import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
+import { Montserrat } from 'next/font/google/'
 
 import './_css/app.scss'
 
+// const montserrat = Montserrat({
+//   subsets: ['latin'],
+//   weight: ['400', '700'],
+//   variable: '--font-body',
+// })
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -23,7 +29,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           <AdminBar />
           {/* @ts-expect-error */}
           <Header />
-          {children}
+          <main className="main">{children}</main>
           {/* @ts-expect-error */}
           <Footer />
         </Providers>
